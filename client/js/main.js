7aa95420c2c972980db43f190752a429737b0648
Ivan Vasilev <i.vasilev@dunice.net>
2021-11-29 18:03:32 +0300

    Add validate with blank input value

--- client/js/main.js
+++ client/js/main.js
@@ -6,7 +6,7 @@ const input = document.getElementById('input');
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  if (input.value) {
+  if (input.value.trim()) {
     socket.emit('chat message', input.value);
     input.value = '';
   }
